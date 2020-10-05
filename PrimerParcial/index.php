00ebd1e5e0abf9fc0d144e83d93cad7c6524e5ce
Jorge <1730093@upv.edu.mx>
2020-10-05 14:39:44 -0500

    Fase 1 Actualizado

--- PrimerParcial/index.php
+++ PrimerParcial/index.php
@@ -1,7 +1,8 @@
 <?php
     //Invoca al controlador y modelo solicitado
 
-    require_once "controller/controller.php";
+    require_once "model/enlaces.php"; 
+    require_once "model/crud.php";
     require_once "controller/controller.php";
     
     //Se crea un nuevo controlador llamando a la plantilla que mostrará al usuario
